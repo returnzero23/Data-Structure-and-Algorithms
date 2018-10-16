f139bf042a5e8a00985a8eb28982e564784bdb60
ReturnZer0 <raofei23@gmail.com>
2018-10-16 22:38:18 +0800

    add InsertionSort

--- src/Sort/sort.cpp
+++ src/Sort/sort.cpp
@@ -1,21 +1,40 @@
 #include <iostream>
 
 template<typename T>
-void BubleSort(T* array, const int size){
+void BubbleSort(T* array, const int size){
     for(int i = 0; i < size; i++){
+        bool flag = false;
         for(int j = 0; j < size - i - 1; j++){
             if(array[j] > array[j+1]){
                 int temp = array[j];
                 array[j] = array[j+1];
                 array[j+1] = temp;
+                flag = true;
             }
         }
+        if(!flag) break;
     }
 };
 
 template<typename T>
 void InsertionSort(T* Array, const int size){
+    if(size <= 1)return;
     
+    for(size_t i = 1; i < size; i++)
+    {
+        int InsertData = Array[i];
+        for(size_t j = 0; j < i; j++)
+        {
+            if(InsertData < Array[j]){
+                for(size_t k = i; k > j; k--)
+                {
+                    Array[k] = Array[k-1];
+                }
+                Array[j] = InsertData;
+                break;
+            }
+        }
+    }
 };
 
 template<typename T>
@@ -24,10 +43,16 @@ void SelectionSort(T* Array, const int size){
 };
 
 int main(){
-    int Array[] = {1,2,3,43,1,4,3,1,34,4,5};
-    BubleSort(Array,11);
+    int Array1[] = {1,2,3,43,1,4,3,1,34,4,5};
+    BubbleSort(Array1,11);
     for(int i = 0 ; i < 11 ; i++){
-        std::cout << Array[i] << std::endl;
+        std::cout << Array1[i] << std::endl;
+    }
+
+    int Array2[] = {1,2,3,43,1,4,3,1,34,4,5};
+    InsertionSort(Array2,11);
+    for(int i = 0 ; i < 11 ; i++){
+        std::cout << Array2[i] << std::endl;
     }
     return 0;
 }
